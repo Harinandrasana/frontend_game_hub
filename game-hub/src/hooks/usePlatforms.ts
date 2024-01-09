ccec72d262b256a70819440152c9eeff63efb35e
Harinandrasana <cheaterrandriasony@gmail.com>
2024-01-09 10:37:50 +0300

    30 : use react query on usePlatforms

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -16,7 +16,7 @@ const usePlatforms = () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: platforms.length, results: platforms },
   });
 
