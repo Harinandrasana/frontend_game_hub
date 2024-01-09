9cdb81f93addd037104338a7c49bd707beeb6658
Harinandrasana <cheaterrandriasony@gmail.com>
2024-01-09 10:00:48 +0300

    rectify image-url error

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "79ae3052ff8d4daa950064b1a1cfe63f",
   },
 });
